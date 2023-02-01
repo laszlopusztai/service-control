e1abf2f6be4d7c78f0b1360f833275aef7daa1ac
Laszlo Pusztai <laszlo.pusztai76@gmail.com>
2023-02-01 11:57:39 +0100

    Added CarType model, table, factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Tag;
+use App\Models\CarType;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,6 +18,12 @@ class DatabaseSeeder extends Seeder
         foreach (['Tag1', 'Tag2', 'Tag3', 'Tag4', 'Tag5', 'Tag6'] as $name) {
             Tag::factory()->create([
                 'name' => $name,
+            ]);
+        }
+
+        foreach (['Subaru', 'Toyota', 'Audi'] as $type) {
+            CarType::factory()->create([
+                'type' => $type,
             ]);
         }
     }
