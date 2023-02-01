95d05ab78462423461231f323afea7ed6d75dbb7
Laszlo Pusztai <laszlo.pusztai76@gmail.com>
2023-02-01 16:41:45 +0100

    Styling login page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,20 +20,6 @@
             <div class="max-w-2xl mx-8 md:mx-auto mt-10">
                 @inertia
             </div>
-
-            <!-- Page Heading -->
-{{--            @if (isset($header))--}}
-{{--                <header class="bg-white shadow">--}}
-{{--                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">--}}
-{{--                        {{ $header }}--}}
-{{--                    </div>--}}
-{{--                </header>--}}
-{{--            @endif--}}
-
-            <!-- Page Content -->
-{{--            <main>--}}
-{{--                {{ $slot }}--}}
-{{--            </main>--}}
         </div>
     </body>
 </html>
